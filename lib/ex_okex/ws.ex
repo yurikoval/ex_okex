ebc1cc2847401842b3daf6d79df2f3a8f1487eee
Long Nguyen <long.polyglot@gmail.com>
2018-12-12 13:29:30 +0800

    Improve code

--- lib/ex_okex/ws.ex
+++ lib/ex_okex/ws.ex
@@ -1,31 +1,38 @@
 defmodule ExOkex.Ws do
+  @moduledoc false
+
   use WebSockex
   require Logger
 
   # Client API
   defmacro __using__(_opts) do
     quote do
+      @base "wss://real.okex.com:10440/websocket/okexapi"
+      @ping_interval Application.get_env(:okex, :ping_interval, 5_000)
+
       def start_link(opts \\ []) do
-        WebSockex.start_link(
-          "wss://real.okex.com:10440/websocket/okexapi",
-          __MODULE__,
-          :fake_state,
-          opts
-        )
+        WebSockex.start_link(@base, __MODULE__, :fake_state, opts)
       end
 
-      def subscribe(client, channel \\ "index/ticker:BTC-USD") do
-        # params = Poison.encode!(%{op: "subscribe", args: ["#{channel}"]})
-        channel = "ok_sub_futureusd_btc_index"
+      def init(_) do
+        ping_me(@ping_interval)
+        {:ok, :fake_state}
+      end
+
+      defp ping_me(interval) do
+        WebSockex.send_frame(self(), {:text, Poison.encode!(%{event: "ping"})})
+        Process.send_after(self(), :ping_me, interval)
+      end
+
+      def handle_info(:schedule, _state) do
+        ping_me()
+        {:noreply, :fake_state}
+      end
+
+      def subscribe(client, channel \\ "ok_sub_futureusd_btc_index") do
         params = Poison.encode!(%{event: "addChannel", channel: channel})
         WebSockex.send_frame(client, {:text, params})
       end
-
-      # V3
-      # def login(client) do
-      #   params = Poison.encode!(%{op: "login", args: auth_params()})
-      #   WebSockex.send_frame(client, {:text, params})
-      # end
 
       # V1
       def login(client) do
@@ -36,7 +43,7 @@ defmodule ExOkex.Ws do
       def auth_params do
         api_key = Application.get_env("okex_api_key")
         secret_key = Application.get_env("okex_secret_key")
-        timestamp = (:os.system_time(:millisecond) / 1000) |> Float.to_string()
+        timestamp = Float.to_string(:os.system_time(:millisecond) / 1000)
         path = "GET/users/self/verify"
         sign_data = "#{timestamp}#{path}"
 
@@ -64,53 +71,11 @@ defmodule ExOkex.Ws do
         compressed_data
         |> :zlib.unzip()
         |> Poison.decode!()
-        |> handle_uncompressed_data()
+        |> handle_response()
       end
 
-      def handle_uncompressed_data(%{"event" => "login", "success" => true}) do
-        Logger.info("Login success!")
-        {:ok, :fake_state}
-      end
-
-      def handle_uncompressed_data(%{"channel" => "addChannel", "data" => data}) do
-        IO.inspect(data)
-        {:ok, :fake_state}
-      end
-
-      # Sample response data
-      # data = %{
-      #   "futureIndex" => "3464.6675",
-      #   "timestamp" => "1544435154968",
-      #   "usdCnyRate" => "6.899"
-      # }
-
-      def handle_uncompressed_data(%{"channel" => _channel, "data" => data}) do
-        IO.inspect(data)
-        {:ok, :fake_state}
-      end
-
-      def handle_uncompressed_data([data]) do
-        handle_uncompressed_data(data)
-      end
-
-      def handle_uncompressed_data(%{"channel" => "login", "data" => data}) do
-        Logger.info("Login success!")
-        {:ok, :fake_state}
-      end
-
-      def handle_uncompressed_data(%{"event" => "subscribe", "subscribe" => channel}) do
-        Logger.info("You're subcribing this channel #{channel}")
-        {:ok, :fake_state}
-      end
-
-      def handle_uncompressed_data(%{"table" => table, "data" => data}) do
-        IO.inspect(data)
-        {:ok, :fake_state}
-      end
-
-      def handle_disconnect(_params, _state) do
-        Logger.info("something went wrong")
-        {:ok, :fake_state}
+      def handle_response(resp) do
+        Logger.info("#{__MODULE__} received response: #{inspect(resp)}")
       end
 
       def handle_disconnect(_, state), do: {:ok, state}
