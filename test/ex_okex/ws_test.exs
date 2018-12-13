e1c68a96b97dbfbae15dcc34272be2d57e43a0db
Yuri Kovalov <yuri@yurikoval.com>
2018-12-13 12:45:06 +0800

    Remove old tests

--- test/ex_okex/ws_test.exs
+++ test/ex_okex/ws_test.exs
@@ -15,17 +15,5 @@ defmodule ExOkex.WsTest do
       {:ok, socket} = WsWrapper.start_link()
       {:ok, socket: socket}
     end
-
-    test "subscribe", %{socket: socket} do
-      :ok = WsWrapper.subscribe(socket)
-
-      Process.sleep(2000)
-    end
-
-    test "login", %{socket: socket} do
-      :ok = WsWrapper.login(socket)
-
-      Process.sleep(1000)
-    end
   end
 end
