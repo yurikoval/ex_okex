8eee422a2f3e124f8dfcf56063af562b7262f742
Long Nguyen <long.polyglot@gmail.com>
2019-01-24 13:58:40 +0700

    Add simple test cases

--- test/ex_okex/ws_test.exs
+++ test/ex_okex/ws_test.exs
@@ -10,10 +10,29 @@ defmodule ExOkex.WsTest do
 
   use ExUnit.Case, async: true
 
+  setup do
+    channels = ["futures/trade:BTC-USD-190904"]
+
+    {:ok, socket} =
+      WsWrapper.start_link(%{
+        channels: channels,
+        require_auth: false,
+        config: %{access_keys: ["OK_1_API_KEY", "OK_1_API_SECRET", "OK_1_API_PASSPHRASE"]}
+      })
+
+    {:ok, socket: socket}
+  end
+
   describe "initial state" do
-    setup do
-      {:ok, socket} = WsWrapper.start_link()
-      {:ok, socket: socket}
+    test "get state", %{socket: socket} do
+      assert :sys.get_state(socket) == %{
+               channels: ["futures/trade:BTC-USD-190904"],
+               config: %{
+                 access_keys: ["OK_1_API_KEY", "OK_1_API_SECRET", "OK_1_API_PASSPHRASE"]
+               },
+               heartbeat: 0,
+               require_auth: false
+             }
     end
   end
 end
