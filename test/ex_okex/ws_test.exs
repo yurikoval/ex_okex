5ef221a76285f6c35288862384c196acb96651af
Yuri Kovalov <yuri@yurikoval.com>
2019-02-08 11:34:32 +0800

    Refactor state retrieval to setup

--- test/ex_okex/ws_test.exs
+++ test/ex_okex/ws_test.exs
@@ -22,12 +22,12 @@ defmodule ExOkex.WsTest do
         config: %{access_keys: ["OK_1_API_KEY", "OK_1_API_SECRET", "OK_1_API_PASSPHRASE"]}
       })
 
-    {:ok, socket: socket}
+    {:ok, socket: socket, state: :sys.get_state(socket)}
   end
 
   describe "initial state" do
-    test "get state", %{socket: socket} do
-      assert :sys.get_state(socket) == %{
+    test "get state", %{state: state} do
+      assert state == %{
                channels: ["futures/trade:BTC-USD-190904"],
                config: %{
                  access_keys: ["OK_1_API_KEY", "OK_1_API_SECRET", "OK_1_API_PASSPHRASE"]
@@ -37,9 +37,8 @@ defmodule ExOkex.WsTest do
              }
     end
 
-    test "pong response from okex", %{socket: socket} do
+    test "pong response from okex", %{state: state} do
       data = {:binary, <<43, 200, 207, 75, 7, 0>>}
-      state = :sys.get_state(socket)
 
       assert capture_log(fn -> WsWrapper.handle_frame(data, state) end) =~
                "received response: \"pong\""
